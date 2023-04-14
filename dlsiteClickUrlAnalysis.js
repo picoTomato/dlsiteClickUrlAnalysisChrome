870d39096bac8b014251ac0658bdb44c8c683aad
picoTomato <pico-tomato@outlook.jp>
2023-04-14 16:34:25 +0900

    初回コミット

---
+++ dlsiteClickUrlAnalysis.js
@@ -0,0 +1,58 @@
+const GET_TITLE_URL = 'https://zero-distance.lsv.jp/get_title.php';
+
+$(function() {
+  // クリック URL に取得ボタンを追加
+  $('#af_report_data th:first').html('URL&nbsp<button class="jsDlsiteClickUrlAnalysis">タイトル取得</button>');
+
+  // タイトル取得ボタン押下時
+  $('.jsDlsiteClickUrlAnalysis').click(function() {
+    var urls = $('#af_report_data tbody').find('td:nth-child(2n+1)');
+    urls.each(function() {
+      var event = $(this);
+      var originalText = event.text();
+      // GET 値の前までを取得
+      var targetPageUrl = originalText.match(/(.*)\/\?/)[1];
+      var matomeNumber = '';
+      var title = '';
+      var matomeTitle = '';
+
+      if (originalText.match(/matome%2F(.*)/)) {
+        matomeNumber = originalText.match(/matome%2F(.*)/)[1];
+        if (matomeNumber.match(/(.*)%3Fdls/)) {
+          matomeNumber = matomeNumber.match(/(.*)%3Fdls/)[1];
+        }
+      };
+
+      // 対象の作品ページのタイトル取得
+      $.ajax({
+        type: 'GET',
+        url: targetPageUrl,
+        data: '',
+        dataType: 'html'
+      }).done(function(data) {
+        title = getTitle(data);
+        // まとめページのタイトル
+        if (matomeNumber !== '') {
+          $.ajax({
+            type: 'GET',
+            url: GET_TITLE_URL,
+            data: {number: matomeNumber},
+            dataType: 'html'
+          }).done(function(data) {
+            matomeTitle = getTitle(data);
+            event.html(originalText + '<br><b>' + title + '</b><br><b>' + matomeTitle + '</b>');
+          });
+        } else {
+          event.html(originalText + '<br><b>' + title + '</b>');
+        }
+      });
+    });
+  });
+
+  /**
+   * html からタイトルを取得する
+   */
+  function getTitle(str) {
+    return str.match(/<title>(.*)<\/title>/)[1];
+  }
+});
